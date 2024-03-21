db87a79606b8219284705318e08601afb7a9a98c
lawrenceDegoma <155027281+lawrenceDegoma@users.noreply.github.com>
2024-03-21 01:44:59 -0700

    Matrix Arithmetic

---
+++ Matrix.h
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#ifndef VECTORASSIGNMENT_MATRIX_H
+#define VECTORASSIGNMENT_MATRIX_H
+
+
+class Matrix{
+public:
+    Matrix();
+    Matrix(int, int);
+    Matrix operator+(Matrix const&);
+    Matrix operator-(Matrix const&);
+    Matrix operator*(Matrix const&);
+    int getRow();
+    int getColumn();
+    void setRow(int);
+    void setColumn(int);
+    int setElement(int, int, int);
+    void printMatrix();
+private:
+    int column;
+    int row;
+    std::vector<std::vector<int>> matrix;
+};
+
+#endif
